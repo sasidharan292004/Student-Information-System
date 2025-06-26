f095ec2696ea8c079336f1c75f7ba330955717dc
Sasidharan S <sasidharanselvakumar9@gmail.com>
2025-06-26 11:12:34 +0530

    Update JDBC.java

--- src/sms_project/JDBC.java
+++ src/sms_project/JDBC.java
@@ -12,8 +12,8 @@ import java.sql.*;
  */
 public class JDBC {
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=SMS;trustServerCertificate=true";
-    private static final String USER = "SASIDHARAN-S";
-    private static final String PASSWORD = "sasisasi";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
     
     public static Connection con()throws Exception
     {
